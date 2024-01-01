5375b5b70310523f8ad37720900e43a171d38d8c
Akoatem <akoatemsam2003@gmail.com>
2024-01-01 10:10:35 +0200

    updated

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -10,7 +10,7 @@ import admin from "../assets/admin.png"
 const ProjectData = [
     {
         imgsrc: ecom,
-        title: "Ecommmece-MERN Stack App",
+        title: "Ecommmece MERN Stack App",
         text: "Full stack ecommerce application with the most recent technologies and has many functionalities, payment gateway, users dashboard, admin dashboard, order tracking, users' authentication, authorization and permission role, JTW token. Backend mongoDB, node js, express js and the frontend react js, javascript, html, css and bootstrap.",
         view: "https://zany-pink-boa.cyclic.app/",
         source: "https://github.com/Akoatem/MERN_STACK_ECOMMERCE.git"
@@ -18,7 +18,7 @@ const ProjectData = [
 
     {
         imgsrc: tour1,
-        title: "Booking Tour-MERN Stack App",
+        title: "Booking Tour MERN Stack App",
         text: " Fully MERN stack tours & travels booking website using react js, node js,  express js, MongoDB, JWT, cookies from scratch with modern UI and UX with admin dashboard to create, update and delete booking.Users are able to create an account, login and book a tour to any destination in the world. The frontend use of javascript, html, css",
         view: "https://frontend-travel-booking.onrender.com",
         source: "https://github.com/Akoatem/frontend_travel_booking-.git"
@@ -26,7 +26,7 @@ const ProjectData = [
 
     {
         imgsrc: ecom1,
-        title: "Django Ecommerce-Full Stack App",
+        title: "Django Ecommerce Full Stack App",
         text: " A complete ecommerce website developed using Django Framework with many functionalities using PayPal API for payment for users and guest users. User registration, login, and reset password with activation link send to your email.This app is developed using Python for backend, Postgres database, and JavaScript, HTML and CSS for frontend",
         view: "https://www.youtube.com/watch?v=BDiIiLXCb7I",
         source: "https://github.com/Akoatem/MVP.git"
@@ -34,7 +34,7 @@ const ProjectData = [
 
     {
         imgsrc: booking,
-        title: "Hotels-MERN Stack App",
+        title: "Hotels airBnB MERN Stack App",
         text: "Hotels BnB application using MERN stack and custom api where users can search for hotels, resort, holiday apartment around the world and book. Admin dashboard to create, update, delete and search hotels, room and users.Users' signup and login with secure JWT token. Backend node js, express, and frontend react, javascript, html, and bootstrap for the admin",
         view: "https://hotel-booking-bnb-app.onrender.com/",
         source: "https://github.com/Akoatem"
@@ -42,14 +42,14 @@ const ProjectData = [
 
     {
         imgsrc: todo,
-        title: "ToDo List-Full Stack App",
+        title: "ToDo List Full Stack App",
         text: "A complete full stack ToDo application which perform all the CRUD operations, users are able to fetch, create, update and delete items from the lists using it own APIs endpoint from the router. This application is fully functional and built with mongoDB for database, express js, node js for the backend and react, javascript, html and css for the frontend.",
         view: "https://full-stack-to-do-app-frontend-two.vercel.app/",
         source: "https://github.com/Akoatem/full_stack_ToDo_app_frontend.git"
     },
     {
         imgsrc: meal,
-        title: "Food Search-Rest API app",
+        title: "Food Search Rest API app",
         text: " An amazing Food search application that is able to display food according to letters, ingredients and meal search with a source display videos where you can watch how to prepare a meal according to the recipes with all directives. This is a complete api application, this app is build using REST APIs, Vue JS 3 & Vite , JavaScript, HTML and Tailwind CSS",
         view: "https://food-search-app-three.vercel.app/",
         source: "https://github.com/Akoatem/Vue_Vite_JS_Food_Search_App.git"
@@ -57,8 +57,8 @@ const ProjectData = [
 
       {
         imgsrc: admin,
-        title: "Admin Dashboard-Frontend App ",
-        text: "An admin dashboard application built from scratch in react JS which can be easily integrated to the backend and client side and can be used to create users, products and show analytics, sale and others. Using frontend language javascript, html and css and material UI in react framework.",
+        title: "Admin Dashboard Frontend App ",
+        text: "An admin dashboard application built from scratch in react JS which can be easily integrated to the backend, mongoDB and client side and can be used to create users, products and show analytics, saleS and others. Using frontend language javascript, html and css and material UI in react framework.",
         view: "https://admindashboard-react-app.onrender.com/",
         source: "https://github.com/Akoatem/Admin_dashboard_react_app.git"
     },
@@ -66,7 +66,7 @@ const ProjectData = [
     {
         imgsrc: gpt3,
         title: "GPT3-Frontend App",
-        text: "This is a simple GPT3 app designed using figma, modern UX and UI in react. A complete frontend app using JavaScript, HTML, CSS, bootstrap and deployed in render.com",
+        text: "This is fully responsive and mobile friendly web application call GPT3 built from scratch using material-UI, figma, modern UX and UI in react. This web application can be used for a blog, portfolio and other services. A complete frontend app using JavaScript, HTML, CSS, bootstrap and deployed in render.com",
         view: "https://gpt3-app-frontend.onrender.com/",
         source: "https://github.com/Akoatem/gpt3_app.git"
     }
